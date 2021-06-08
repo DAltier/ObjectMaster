af2e4b8bcbb18c07b0e32d47e7781ccbb6f4ab10
DAltier <danaaltier@hotmail.com>
2021-06-08 15:36:13 -0700

    Part II completed

---
+++ src/com/codingdojo/objectmaster/Samurai.java
@@ -0,0 +1,31 @@
+package com.codingdojo.objectmaster;
+
+public class Samurai extends Human {
+	
+	private static int counter = 0;
+	
+	
+	public Samurai() {
+		this.health = 200;
+		counter++;
+	}
+	
+	
+	public Samurai deathBlow(Human target) {
+		target.health = 0;
+		this.health = this.health/2;
+		return this;
+	}
+	
+	
+	public Samurai meditate() {
+		this.health += this.health/2;
+		return this;
+	}
+	
+	
+	public int howMany() {
+		return counter;
+	}
+
+}
